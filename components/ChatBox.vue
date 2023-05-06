54389715cd3aa28209bcc128a786679deba67761
Keith Jordan <keith.jordan@healthcareintegrations.com>
2023-05-06 02:45:08 -0400

    exercise 1

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -1,3 +1,136 @@
+<script setup lang="ts">
+import { nanoid } from "nanoid";
+import { Message, User } from "~~/types";
+const props = withDefaults(
+  defineProps<{
+    messages: Message[];
+    users: User[];
+    me: User;
+    usersTyping?: User[];
+  }>(),
+  {
+    usersTyping: () => [],
+  }
+);
+
+defineEmits<{
+  (e: "newMessage", payload: Message): void;
+}>();
+
+const open = ref(false);
+
+function getUser(id: string) {
+  return props.users.find((user) => user.id === id);
+}
+
+// put focus on input area  whenever chatbox is opened
+const input = ref();
+watch(open, () => {
+  if (!open.value) return;
+  nextTick(() => {
+    (input.value as HTMLInputElement).focus();
+  });
+});
+
+// keep messages anchored to bottom
+const messageBox = ref();
+watch(
+  () => props.messages,
+  () => {
+    nextTick(
+      // nextTick(): allows you to execute code after you have changed some data and Vue.js has updated the virtual DOM 
+      // based on your data change, but before the browser has rendered that change on the page. in this case-> after
+      // new message is submitted, wait unit DOM is rendered then scroll it one message box up.
+      () => (messageBox.value.scrollTop = messageBox.value.scrollHeight)
+    );
+  },
+  { deep: true }
+);
+</script>
 <template>
-  <!-- Code out the chatbox for Exercise 1 here -->
+  <div class="fixed bottom-[10px] right-[10px]">
+    <!-- Open Chat nbutton. in lower right of screen. @click -> open the chat box.  hide button if open is true -->
+    <!-- the button is a component named IconChat -->
+    <button v-show="!open" @click="open = true" class="bg-blue-500 p-3 rounded">
+      <IconChat class="h-8 w-8 text-white" />
+    </button>
+    <!-- Div that holds chatbox. use v-if="open" to show the div -->
+    <div
+      v-if="open"
+      class="box bg-gray-300 dark:bg-gray-800 rounded w-[450px] overflow-hidden"
+    >
+      <!-- Header -->
+      <header
+        class="dark:bg-gray-900 bg-gray-200 px-4 flex justify-between items-center"
+      >
+        Customer Support Chat
+        <button class="p-4 pr-0" @click="open = false">
+          <!-- Down arrow to close chatbox. @click set "open" = false [hide box div] -->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </button>
+      </header>
+      <!-- Messages -->
+      <div class="messages p-4 overflow-y-scroll max-h-[80vh]" ref="messageBox">
+        <div v-if="!messages.length" class="text-center w-[350px] m-auto">
+          <strong class="text-lg">Chat with Botman!</strong>
+          <p>Our A.I. powered assistant</p>
+          <strong class="block mt-10">Go ahead and ask us something:</strong>
+          <ul class="list-inside list-disc text-left">
+            <li>What is social media post generator?</li>
+            <li>How can I get human support?</li>
+            <li>How was this tool built?</li>
+          </ul>
+        </div>
+        <ChatBubble
+          v-for="message in messages"
+          :key="message.id"
+          :message="message"
+          :user="getUser(message.userId)"
+          :my-message="message.userId === me.id"
+        />
+
+        <ChatBubble v-for="user in usersTyping" :key="user.id" :user="user">
+          <AppLoading />
+        </ChatBubble>
+      </div>
+      <!-- Footer -->
+      <footer class="p-2">
+        <input
+          ref="input"
+          class="input w-full px-2 block"
+          type="text"
+          placeholder="Type your message"
+          @keypress.enter="
+            $emit('newMessage', {
+              id: nanoid(),
+              userId: me.id,
+              createdAt: new Date(),
+              text: ($event.target as HTMLInputElement).value,
+            });
+            ($event.target as HTMLInputElement).value = '';
+          "
+        />
+
+        <div class="h-6 py-1 px-2 text-sm italic">
+          <span v-if="usersTyping.length">
+            {{ usersTyping.map((user) => user.name).join(" and ") }}
+            {{ usersTyping.length === 1 ? "is" : "are" }} typing
+          </span>
+        </div>
+      </footer>
+    </div>
+  </div>
 </template>
